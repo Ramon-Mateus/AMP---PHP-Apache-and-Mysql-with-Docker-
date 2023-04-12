59f952b90ccaae3eff8982032aaf51be1dbbc360
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2023-04-12 17:53:01 -0300

    First Commit

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+  $servername = "database";
+  $username = "myuser";
+  $password = "mypassword";
+  $dbname = "mydb";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully to MySQL!";
+?>
